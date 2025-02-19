b12c8ae652020b3d04a5465fdcd7310a5db638a0
CypherpunkSamurai <CypherpunkSamurai@users.noreply.github.com>
2025-02-19 16:38:24 +0530

    docker api hello world
    docker api hello world. test image pull alpine:latest
    
    Signed-off-by: CypherpunkSamurai <CypherpunkSamurai@users.noreply.github.com>

--- NOTES.md
+++ NOTES.md
@@ -12,3 +12,18 @@ I've decided to use the following modules cause they look best for use case.
 
 - [github.com/goccy/go-yaml](https://github.com/goccy/go-yaml)
 - [github.com/docker/docker/client](https://github.com/docker/docker/client)
+
+## 2025-02-19 16:35:45 - Lets Try the Docker SDK Code Snippet from the Website
+
+Creating a Docker client is pretty easy. I will be using the Docker SDK from [here](https://docs.docker.com/reference/api/engine/sdk/examples/). The SDK is available in Python, Go, and JavaScript.
+
+The golang version requires that you manually fetch all the dependencies as `go mod tidy` results in lot of errors. [googleapis/go-genproto#1015](https://github.com/googleapis/go-genproto/issues/1015#issuecomment-2380323686).
+
+```shell
+go get "github.com/docker/docker/api/types/container"
+go get "github.com/docker/docker/api/types/image"
+go get "github.com/docker/docker/client"
+go get "github.com/docker/docker/pkg/stdcopy"
+```
+
+We need a working docker daemon and need to run this to pull an `alpine:latest` container.
--- main.go
+++ main.go
@@ -1,8 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"io"
+	"os"
 
-// main
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+)
+
 func main() {
-	fmt.Println("Hello, World!")
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine:latest", image.PullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(os.Stdout, reader)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine:latest",
+		Cmd:   []string{"echo", "hello world"},
+	}, nil, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		panic(err)
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, container.LogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
 }
